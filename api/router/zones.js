bd71a34c3026bf44583d81c0839c01caf1750501
RP97 <rashmi9797@outlook.com>
2018-06-19 19:08:40 +0530

    Add files via upload

---
+++ api/router/zones.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const Zone = require('./../models/zone');
+
+// view details of all zones
+
+router.get('/viewZone', (req, res,next) => {
+    Zone.find()
+    .exec()
+    .then(result => {
+        if(result.length > 0) {
+            res.status(200).json({result});
+        }
+        else {
+            res.status(404).json({
+                message: 'No entries found.....'
+            })
+        }
+        
+    })
+    .catch(err => {
+        consol.log(err)
+        error: err
+    });
+});
+
+//view entry of a Zone
+router.get('/view/:zone_Id', (req, res,next) => {
+    Zone.find({zone_Id: req.params._id})
+    .exec()
+    .then(result => {
+        if(result.length > 0)
+        {
+            res.status(200).json(result)
+        }
+        else {
+            res.status(404).json({
+                message: 'entry not found....'
+            })
+        }
+    })
+});
+
+module.exports = router;
